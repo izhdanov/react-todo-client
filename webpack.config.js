6ba670ece2d3b3195c1fe3809fe0919f9aba3958
zhdanov <zhdanov.iv@esphere.ru>
2018-02-13 23:45:33 +0300

    Init

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+module.exports = {
+    entry: {
+        app: "./src/index.js"
+    },
+    plugins: [
+        new CleanWebpackPlugin(["dist"]),
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
+    ],
+    devtool: "inline-source-map",
+    devServer: {
+        contentBase: './dist',
+        port: 9005
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            camelCase: true,
+                            sourceMap: true
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+};
